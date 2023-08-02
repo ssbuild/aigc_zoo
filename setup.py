8086fb0902bb71641339a0adc15eb7f803661092
ssbuild <462304@qq.cn>
2023-08-02 16:18:50 +0800

    requirement
    Signed-off-by: ssbuild <462304@qq.cn>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 install_requires = [
     # 'deep_training>=0.1.13,<=0.1.15',
-    'deep_training ~= 0.1.13.post0',
+    'deep_training ~= 0.1.13.post1',
     'numpy_io>=0.0.8',
 ]
 
