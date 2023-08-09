04ce2ae792c844fea7c8bacb066d7e201ef5f00b
ssbuild <462304@qq.com>
2023-08-09 22:20:50 +0800

    0.1.16
    Signed-off-by: ssbuild <462304@qq.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ install_requires = [
 if __name__ == '__main__':
     setup(
         name='aigc_zoo',
-        version='0.1.15',
+        version='0.1.16',
         description='AIGC zoo',
         long_description='torch_training: https://github.com/ssbuild/aigc_zoo.git',
         license='Apache License 2.0',
