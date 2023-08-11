5aabde8a29ffc6ece13ab3053cbe05fb8124eed7
ssbuild <462304@qq.com>
2023-08-11 20:33:27 +0800

    update qwen config
    Signed-off-by: ssbuild <462304@qq.com>

--- src/aigc_zoo/model_zoo/qwen/llm_model.py
+++ src/aigc_zoo/model_zoo/qwen/llm_model.py
@@ -28,37 +28,6 @@ class MyQWenLMHeadModel(QWenLMHeadModel):
     def __init__(self,config):
         super(MyQWenLMHeadModel, self).__init__(config)
 
-    def generate(
-            self,
-            inputs: Optional[torch.Tensor] = None,
-            generation_config: Optional[GenerationConfig] = None,
-            logits_processor: Optional[LogitsProcessorList] = None,
-            **kwargs,
-    ) -> Union[GenerateOutput, torch.LongTensor]:
-        # Process stop_words_ids.
-        stop_words_ids = kwargs.pop("stop_words_ids", None)
-        if stop_words_ids is None and generation_config is not None:
-            stop_words_ids = getattr(generation_config, "stop_words_ids", None)
-        if stop_words_ids is None:
-            stop_words_ids = getattr(self.generation_config, "stop_words_ids", None)
-
-        if stop_words_ids is not None:
-            stop_words_logits_processor = StopWordsLogitsProcessor(
-                stop_words_ids=stop_words_ids,
-                eos_token_id=self.generation_config.eos_token_id,
-            )
-            if logits_processor is None:
-                logits_processor = LogitsProcessorList([stop_words_logits_processor])
-            else:
-                logits_processor.append(stop_words_logits_processor)
-
-        return super().generate(
-            inputs,
-            generation_config=generation_config,
-            logits_processor=logits_processor,
-            **kwargs,
-        )
-
     def chat(
             self,
             tokenizer: PreTrainedTokenizer,
@@ -78,12 +47,15 @@ class MyQWenLMHeadModel(QWenLMHeadModel):
         if stop_words_ids is None:
             stop_words_ids = []
 
+        max_window_size = kwargs.pop('max_window_size', None)
+        if max_window_size is None:
+            max_window_size = self.generation_config.max_window_size
         raw_text, context_tokens = make_context(
             tokenizer,
             query,
             history=history,
             system=system,
-            max_window_size=6144,
+            max_window_size=max_window_size,
             chat_format=self.generation_config.chat_format,
         )
 
@@ -106,6 +78,7 @@ class MyQWenLMHeadModel(QWenLMHeadModel):
             context_length=len(context_tokens),
             chat_format=self.generation_config.chat_format,
             verbose=False,
+            errors='replace',
         )
 
         if append_history:
@@ -130,13 +103,16 @@ class MyQWenLMHeadModel(QWenLMHeadModel):
         if not isinstance(stop_words_ids, list):
             stop_words_ids = [stop_words_ids]
 
+        max_window_size = kwargs.get('max_window_size', None)
+        if max_window_size is None:
+            max_window_size = self.generation_config.max_window_size
         logits_processor = kwargs.pop('logits_processor', None)
         raw_text, context_tokens = make_context(
             tokenizer,
             query,
             history=history,
             system=system,
-            max_window_size=6144,
+            max_window_size=max_window_size,
             chat_format=self.generation_config.chat_format,
         )
 
@@ -171,6 +147,37 @@ class MyQWenLMHeadModel(QWenLMHeadModel):
                 yield tokenizer.decode(outputs, skip_special_tokens=True, errors='ignore')
 
         return stream_generator()
+
+    def generate(
+            self,
+            inputs: Optional[torch.Tensor] = None,
+            generation_config: Optional[GenerationConfig] = None,
+            logits_processor: Optional[LogitsProcessorList] = None,
+            **kwargs,
+    ) -> Union[GenerateOutput, torch.LongTensor]:
+        # Process stop_words_ids.
+        stop_words_ids = kwargs.pop("stop_words_ids", None)
+        if stop_words_ids is None and generation_config is not None:
+            stop_words_ids = getattr(generation_config, "stop_words_ids", None)
+        if stop_words_ids is None:
+            stop_words_ids = getattr(self.generation_config, "stop_words_ids", None)
+
+        if stop_words_ids is not None:
+            stop_words_logits_processor = StopWordsLogitsProcessor(
+                stop_words_ids=stop_words_ids,
+                eos_token_id=self.generation_config.eos_token_id,
+            )
+            if logits_processor is None:
+                logits_processor = LogitsProcessorList([stop_words_logits_processor])
+            else:
+                logits_processor.append(stop_words_logits_processor)
+
+        return super().generate(
+            inputs,
+            generation_config=generation_config,
+            logits_processor=logits_processor,
+            **kwargs,
+        )
 
 class MyTransformerForQwen(TransformerBase):
     def __init__(self, *args,**kwargs):
