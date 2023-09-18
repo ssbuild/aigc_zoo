fcebecc5f41ef5c755e976041e7a62f79f207b31
ssbuild <462304@qq.com>
2023-09-18 22:51:29 +0800

    dpo
    Signed-off-by: ssbuild <462304@qq.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ install_requires = [
 if __name__ == '__main__':
     setup(
         name='aigc_zoo',
-        version='0.2.2.post0',
+        version='0.2.3.rc0',
         description='AIGC zoo',
         long_description='torch_training: https://github.com/ssbuild/aigc_zoo.git',
         license='Apache License 2.0',
--- src/aigc_zoo/model_zoo/llm/dpo_model.py
+++ src/aigc_zoo/model_zoo/llm/dpo_model.py
@@ -2,13 +2,78 @@
 # @Time    : 2023/5/12 20:41
 # @Author  : tk
 # @FileName: llm_model
+from typing import Dict, Tuple, Union, List
+import torch
 from deep_training.nlp.layers.rope_scale.patch import *
 from deep_training.nlp.models.transformer import TransformerForCausalLM
-
+from deep_training.nlp.losses.loss_dpo import dpo_loss
+from torch import nn
 from ...utils.transformer_utils import hf_decorator
 from ...weight.modelweighter import *
 import logging
 logger = logging.getLogger(__name__)
+
+
+def pad_to_length(tensor: torch.Tensor, length: int, pad_value: Union[int, float], dim: int = -1) -> torch.Tensor:
+    if tensor.size(dim) >= length:
+        return tensor
+    else:
+        pad_size = list(tensor.shape)
+        pad_size[dim] = length - tensor.size(dim)
+        return torch.cat([tensor, pad_value * torch.ones(*pad_size, dtype=tensor.dtype, device=tensor.device)], dim=dim)
+
+def concatenated_inputs(batch: Dict[str, Union[List, torch.LongTensor]]) -> Dict[str, torch.Tensor]:
+    """Concatenate the chosen and rejected inputs into a single tensor.
+
+    Args:
+        batch: A batch of data. Must contain the keys 'chosen_input_ids' and 'rejected_input_ids', which are tensors of shape (batch_size, sequence_length).
+
+    Returns:
+        A dictionary containing the concatenated inputs under the key 'concatenated_input_ids'.
+    """
+    max_length = max(batch['chosen_input_ids'].shape[1], batch['rejected_input_ids'].shape[1])
+    concatenated_batch = {}
+    for k in batch:
+        if k.startswith('chosen') and isinstance(batch[k], torch.Tensor):
+            pad_value = -100 if 'labels' in k else 0
+            concatenated_key = k.replace('chosen', 'concatenated')
+            concatenated_batch[concatenated_key] = pad_to_length(batch[k], max_length, pad_value=pad_value)
+    for k in batch:
+        if k.startswith('rejected') and isinstance(batch[k], torch.Tensor):
+            pad_value = -100 if 'labels' in k else 0
+            concatenated_key = k.replace('rejected', 'concatenated')
+            concatenated_batch[concatenated_key] = torch.cat((
+                concatenated_batch[concatenated_key],
+                pad_to_length(batch[k], max_length, pad_value=pad_value),
+            ), dim=0)
+    return concatenated_batch
+
+def _get_batch_logps(logits: torch.FloatTensor, labels: torch.Tensor, average_log_prob: bool = False) -> torch.FloatTensor:
+    """Compute the log probabilities of the given labels under the given logits.
+
+    Args:
+        logits: Logits of the model (unnormalized). Shape: (batch_size, sequence_length, vocab_size)
+        labels: Labels for which to compute the log probabilities. Label tokens with a value of -100 are ignored. Shape: (batch_size, sequence_length)
+        average_log_prob: If True, return the average log probability per (non-masked) token. Otherwise, return the sum of the log probabilities of the (non-masked) tokens.
+
+    Returns:
+        A tensor of shape (batch_size,) containing the average/sum log probabilities of the given labels under the given logits.
+    """
+    assert logits.shape[:-1] == labels.shape
+
+    labels = labels[:, 1:].clone()
+    logits = logits[:, :-1, :]
+    loss_mask = (labels != -100)
+
+    # dummy token; we'll ignore the losses on these tokens later
+    labels[labels == -100] = 0
+
+    per_token_logps = torch.gather(logits.log_softmax(-1), dim=2, index=labels.unsqueeze(2)).squeeze(2)
+
+    if average_log_prob:
+        return (per_token_logps * loss_mask).sum(-1) / loss_mask.sum(-1)
+    else:
+        return (per_token_logps * loss_mask).sum(-1)
 
 class TransformerDPOForLM(TransformerForCausalLM):
     def __init__(self, *args,ref_model=None,beta=0.1,ref_free=False, **kwargs):
@@ -39,9 +104,32 @@ class TransformerDPOForLM(TransformerForCausalLM):
 
     def set_ref_model(self, ref_model):
         self.ref_model = ref_model
+
+    def forward_logits(self, model: nn.Module, batch: Dict[str, Union[List, torch.LongTensor]]) -> Tuple[
+        torch.FloatTensor, torch.FloatTensor]:
+        """Run the given model on the given batch of inputs, concatenating the chosen and rejected inputs together.
+
+           We do this to avoid doing two forward passes, because it's faster for FSDP.
+        """
+        concatenated_batch = concatenated_inputs(batch)
+        all_logits = model(concatenated_batch['concatenated_input_ids'],
+                           attention_mask=concatenated_batch['concatenated_attention_mask']).logits.to(torch.float32)
+        all_logps = _get_batch_logps(all_logits, concatenated_batch['concatenated_labels'], average_log_prob=False)
+        chosen_logps = all_logps[:batch['chosen_input_ids'].shape[0]]
+        rejected_logps = all_logps[batch['chosen_input_ids'].shape[0]:]
+        return chosen_logps, rejected_logps
+
     def compute_loss(self, *args, **batch) -> tuple:
-        raise NotImplemented
-        # return self.model(*args, beta=self.beta,reference_free=self.ref_free,**batch)
+        chosen_logps, rejected_logps = self.forward_logits(model=self.backbone,batch=batch)
+        returns = tuple()
+        if self.training:
+            with torch.no_grad():
+                ref_chosen_logps, ref_rejected_logps = self.forward_logits(model=self.ref_model,batch=batch)
+            loss = dpo_loss(chosen_logps, rejected_logps,ref_chosen_logps, ref_rejected_logps,beta=self.beta,reference_free=self.ref_free)
+            returns += ({"loss":loss},)
+        returns += (chosen_logps, rejected_logps)
+        return returns
+
 
 class MyTransformerDPO(TransformerDPOForLM, ModelWeightMixin, with_pl=True):
     @hf_decorator
