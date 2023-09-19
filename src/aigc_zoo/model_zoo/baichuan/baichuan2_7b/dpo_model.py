59e7228f3fa455cc4224cb07367dd2385e8121b6
ssbuild <462304@qq.com>
2023-09-20 01:18:55 +0800

    DPO
    Signed-off-by: ssbuild <462304@qq.com>

--- src/aigc_zoo/model_zoo/baichuan/baichuan2_7b/dpo_model.py
+++ src/aigc_zoo/model_zoo/baichuan/baichuan2_7b/dpo_model.py
@@ -8,7 +8,7 @@ import torch
 from deep_training.nlp.models.baichuan2_7b.modeling_baichuan import BaichuanForCausalLM,BaichuanConfig,setup_model_profile
 from deep_training.nlp.models.transformer_base import TransformerBase
 from transformers import GenerationConfig
-
+from ....utils.dpo_utils import DpoModule
 from ....utils.transformer_utils import hf_decorator
 from ....weight.modelweighter import *
 from .tokenization_baichuan import BaichuanTokenizer
@@ -21,9 +21,9 @@ class MyBaichuanForCausalLM(BaichuanForCausalLM):
     ...
 
 
-class TransformerForLM(DpoModule,TransformerBase):
+class TransformerDPOForLM(DpoModule,TransformerBase):
     def __init__(self, *args,ref_model=None,beta=0.1,ref_free=False,**kwargs):
-        super(TransformerForLM, self).__init__(*args,**kwargs)
+        super(TransformerDPOForLM, self).__init__(*args,**kwargs)
         self.set_model(self.from_pretrained(MyBaichuanForCausalLM, *args, **kwargs))
         self.beta = beta
         self.ref_free = ref_free
@@ -42,9 +42,6 @@ class TransformerForLM(DpoModule,TransformerBase):
         # self.model.lm_head = CastOutputToFloat(self.model.lm_head)
 
 
-
-
-
     def enable_input_require_grads(self):
         # setattr(self.model, 'model_parallel', True)
         # setattr(self.model, 'is_parallelizable', True)
@@ -53,12 +50,12 @@ class TransformerForLM(DpoModule,TransformerBase):
 
 
 
-class MyTransformer(TransformerForLM, ModelWeightMixin, with_pl=True):
+class MyTransformerDPO(TransformerDPOForLM, ModelWeightMixin, with_pl=True):
     @hf_decorator
     def __init__(self, *args,new_num_tokens=None, **kwargs):
         lora_args: LoraConfig = kwargs.pop('lora_args', None)
         prompt_args: PromptLearningConfig = kwargs.pop('prompt_args', None)
-        super(MyTransformer, self).__init__(*args, **kwargs)
+        super(MyTransformerDPO, self).__init__(*args, **kwargs)
         self.lora_args = lora_args
         self.prompt_args = prompt_args
         #可能扩充词表
@@ -118,7 +115,7 @@ class MyTransformer(TransformerForLM, ModelWeightMixin, with_pl=True):
             return [(self.backbone, lr)]
         elif self.prompt_args and self.prompt_args.with_prompt:
             return [(self.backbone, lr)]
-        return super(MyTransformer, self).get_model_lr(model, lr)
+        return super(MyTransformerDPO, self).get_model_lr(model, lr)
 
 
     def get_llm_model(self) -> Optional[Union[BaichuanForCausalLM,Any]]:
